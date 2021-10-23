186d7c5b9d5d8e8a3205dd3bb9e178d843ef0788
Jessica Uppal <jessica@Jessicas-MacBook-Pro.local>
2021-10-23 16:14:40 +0100

    code added

---
+++ Leaflet-Step-1/static/logic.js
@@ -0,0 +1,119 @@
+//URL to earthquake json data
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+var earthquakemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution:
+    "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,    
+maxZoom: 18,
+zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+  });
+      
+// Add streetmap and earthquakes layers 
+    var myMap = L.map("map", {
+      center: [37.09, -95.71],
+      zoom: 3
+    });
+earthquakemap.addTo(myMap);
+
+// function to determine marker size based on magnitude
+function markerSize(magnitude) {
+    return magnitude * 5;
+}
+
+// function to return the colour based on magnitude
+function markerColor(magnitude) {
+  if (magnitude > 4) {
+    return 'red'
+  } else if (magnitude > 3) {
+    return 'orange'
+  } else if (magnitude > 2) {
+    return 'yellow'
+  } else {
+    return 'green'
+  }
+}
+
+// function for opacity based on earthquakes depth
+function markerOpacity(depth) {
+  if (depth > 6) {
+    return .99
+  } else if (depth > 5) {
+    return .80
+  } else if (depth > 4) {
+    return .70
+  } else if (depth > 3) {
+    return .60
+  } else if (depth > 2) {
+    return .50
+  } else if (depth > 1) {
+    return .40
+  } else {
+    return .30
+  }
+}
+
+//Getting our GeoJSON data
+d3.json(queryUrl).then(function(data) {
+  // Creating a GeoJSON layer with the data
+  var earthquakes = L.geoJSON(data.features, {
+    onEachFeature : addPopup,
+    pointToLayer: addMarker });
+    earthquakes.addTo(myMap);
+    function addMarker(feature, location) {
+        var options = {
+          stroke: false,
+          fillOpacity: markerOpacity(feature.properties.mag),
+          color: markerColor(feature.properties.mag),
+          fillColor: markerColor(feature.properties.mag),
+          radius: markerSize(feature.properties.mag)
+        };
+      
+        return L.circleMarker(location, options);
+    
+      
+      }
+      // Create popup function with earthquake information
+function addPopup(feature, layer) {
+    return layer.bindPopup(`<h3> ${feature.properties.place} </h3> <hr> <h4>Magnitude: ${feature.properties.mag} </h4> <p> ${Date(feature.properties.time)} </p>`);
+}
+
+    // Create and add legend on map
+    // Set up the legend.
+  var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var limits = earthquakes.options.limits;
+    var colors = earthquakes.options.colors;
+    var labels = [];
+
+    // Add the minimum and maximum.
+    var legendInfo = "<h1>Median Income</h1>" +
+      "<div class=\"labels\">" +
+        "<div class=\"min\">" + limits[0] + "</div>" +
+        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+      "</div>";
+
+    div.innerHTML = legendInfo;
+
+    limits.forEach(function(limit, index) {
+      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    });
+
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+  };
+
+  // Adding the legend to the map
+  legend.addTo(myMap);
+
+ 
+
+  
+
+options.addTo(myMap);
+
+
+  });
